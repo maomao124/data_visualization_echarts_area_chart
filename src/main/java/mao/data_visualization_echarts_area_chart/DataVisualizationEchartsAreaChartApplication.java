4f066a2f57c7f53dba10a032bc1f644798c25796
mao <1296193245@qq.com>
2023-06-13 23:47:25 +0800

    data_visualization_echarts_area_chart

---
+++ src/main/java/mao/data_visualization_echarts_area_chart/DataVisualizationEchartsAreaChartApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_area_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationEchartsAreaChartApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationEchartsAreaChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_echarts_area_chart/DataVisualizationEchartsAreaChartApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_area_chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationEchartsAreaChartApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
